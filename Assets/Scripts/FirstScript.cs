0cfa9a6640d5f9f166ccc265526d4bac27d334f8
connor1688 <119415296+connor1688@users.noreply.github.com>
2022-12-27 21:19:57 +0800

    Update 認識腳本

---
+++ Assets/Scripts/FirstScript.cs
@@ -0,0 +1,13 @@
+﻿// 單行註解：說明、解釋、文字說明的用途
+// HH 2022.12.27 O<Z...  
+// 程式不會執行
+// 程式上下、後(悔學程式了)
+
+using UnityEngine;  // 引用命名空間(倉庫) Unity 遊戲引擎 (API)
+
+// class 類別 等同於腳本 Script
+// class 後面接[FirstScript]為腳本名稱，必須與檔案名稱[左上]相同
+public class FirstScript : MonoBehaviour
+{
+    // 腳本都會有一對大括號，放置腳本內容
+}
